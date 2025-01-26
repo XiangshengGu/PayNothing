e4af1eceac40f3df7228ed8061770528c263bcd4
chenzhang1709 <czhang708@gatech.edu>
2025-01-26 01:47:31 -0500

    Modify misc files

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+//const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push('cjs');
+
+module.exports = defaultConfig;
