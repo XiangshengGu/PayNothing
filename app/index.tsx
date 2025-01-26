e677a58215ef1ce1c8040153d6e04e80f6fff38a
chenzhang1709 <czhang708@gatech.edu>
2025-01-26 01:50:26 -0500

    Add index.tsx file

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './login'
+
+const Stack = createNativeStackNavigator();
+
+export default function Index() {
+  return (
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      </Stack.Navigator>
+  );
+}
