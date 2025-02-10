dce68e767b674ef912b130e1fbd837042b925cf5
Gu <74333614+XiangshengGu@users.noreply.github.com>
2025-02-10 18:07:36 -0500

    Configure Firebase as the cloud servers and Data storage

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,3 +1,25 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA4o2bhIDA5W4xiy_e8vIQ1bJY-Ed6r9GI",
+//   authDomain: "paynothingapp.firebaseapp.com",
+//   projectId: "paynothingapp",
+//   storageBucket: "paynothingapp.firebasestorage.app",
+//   messagingSenderId: "115198796724",
+//   appId: "1:115198796724:web:6959329825d78b13f788e1",
+//   measurementId: "G-GX0CM29V3M"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
